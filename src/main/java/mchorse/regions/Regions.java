a360e8ce4a0449de700eee6e86d97d9720dd1634
Jimmy Jenkins <mchorselessrider@gmail.com>
2016-11-26 09:14:49 +0000

    Increase the version number

--- src/main/java/mchorse/regions/Regions.java
+++ src/main/java/mchorse/regions/Regions.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class Regions
 {
     public static final String MODID = "regions";
-    public static final String VERSION = "0.1.4";
+    public static final String VERSION = "0.1.5";
 
     /**
      * Server file
